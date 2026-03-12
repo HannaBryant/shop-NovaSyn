71a91343b83ef13c3f3ec85710ecee0fea14d27b
Hanna Bryant <hannab329@gmail.com>
2026-03-12 17:59:51 -0500

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <>
+    
     <main className="text-center mt-16 px-6">
 
         <h1 className="text-4xl font-bold text-gray-800 mb-4">
@@ -12,7 +13,7 @@ export const Home = () => {
         <p className="text-lg text-gray-600">
           At NovaSyn, we strive to advance the future of Human Technology, improving life experiences one device at a time.
         </p>
-
+        
       </main>
     </>
   )
